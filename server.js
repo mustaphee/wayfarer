137f10af0ce6519aa8733442d8f2647077896dcf
Yusuff Mustapha <mustaphee94@gmail.com>
2019-07-13 07:59:11 +0100

    refactor(server): changed export method

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-import express from 'express'
-import logger from 'morgan'
+import express from 'express';
+import logger from 'morgan';
 import 'dotenv/config';
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-//Basic config
-app.use(logger('combined'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+// Basic config
+app.use(logger('combined'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, () => console.log(`Super Wayfarer app listening on  ${PORT}!`))
+app.listen(PORT, () => console.log(`Super Wayfarer app listening on  ${PORT}!`));
+
+export default app;
